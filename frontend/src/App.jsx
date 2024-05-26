4dbd411521f256051250fd7089bf6bdbc2254c02
nimendra-ag <nimendraamiththa1@gmail.com>
2024-05-26 13:18:59 +0530

    Routes in the navbar are working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import Navbar from './Components/Navbar/Navbar'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './Pages/Home/Home';
+import Categories from './Pages/Categories/Categories';
+import Watchlist from './Pages/Watchlist/Watchlist';
+import Account from './Pages/Account/Account';
+import Reviews from './Pages/Reviews/Reviews';
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/categories' element={<Categories />} />
+          <Route path='/watchlist' element={<Watchlist />} />
+          <Route path='/account' element={<Account />} />
+          <Route path='/reviews' element={<Reviews />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Logo from '../../assets/Images/Sabzi.png'
+import './Navbar.css'
+const Navbar = () => {
+  return (
+   <div className="container">
+    <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+    <a
+          href="/"
+          className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none"
+        >
+            <img src={Logo} alt="" style={{width: '110px', height:'40px'}}/>
+        </a>
+
+      <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+        <li><div href="#" className="nav-link px-2 link-secondary me-3"><Link style={{textDecoration: 'none'}} to='/watchlist'>Watchlist</Link></div></li>
+        <li><div href="#" className="nav-link px-2 link-dark"><Link style={{textDecoration: 'none'}} to='/reviews'>Reviews</Link></div></li>
+        <li><div href="#" className="nav-link px-2 link-dark"><Link style={{textDecoration: 'none'}} to='/categories'>Categories</Link></div></li>
+        <li><div href="#" className="nav-link px-2 link-secondary"><Link style={{textDecoration: 'none'}} to='/account'>Account</Link></div></li>
+      </ul>
+
+      <div className="col-md-3 text-end">
+        <button type="button" className="btn btn-outline-primary me-2">Login</button>
+      </div>
+    </header>
+  </div>
+  )
+}
+
+export default Navbar
