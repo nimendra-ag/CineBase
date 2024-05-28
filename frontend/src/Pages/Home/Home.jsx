2e2b8b831459f655cbcfbc6ab3991babe0f4265e
nimendra-ag <nimendraamiththa1@gmail.com>
2024-05-28 22:53:09 +0530

    caroseul added but not working as expected.

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -2,7 +2,27 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div id="carouselExampleFade" className="carousel slide carousel-fade" data-ride="carousel">
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <img src="https://picsum.photos/1920/1080" className="d-block w-100" alt="..." />
+        </div>
+        <div className="carousel-item">
+          <img src="https://picsum.photos/1920/1080?blur" className="d-block w-100" alt="..." />
+        </div>
+        <div className="carousel-item">
+          <img src="https://picsum.photos/1920/1080?grayscale" className="d-block w-100" alt="..." />
+        </div>
+      </div>
+      <a className="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+      </a>
+      <a className="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+      </a>
+    </div>
+
+
   )
 }
 
