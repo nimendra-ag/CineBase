82ffc1852f0505563ca156572ebd73ce2d180063
nimendra-ag <nimendraamiththa1@gmail.com>
2024-06-29 14:36:25 +0530

    dropdown button in the navbar is ok

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -20,22 +20,22 @@ const Navbar = () => {
           <div className="collapse navbar-collapse" id="navbarScroll">
             <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style={{ '--bs-scroll-height': '100px' }}>
               <li><div className="nav-link px-2 link-secondary nav-item"><Link className="nav-link active" style={{ textDecoration: 'none' }} to='/watchlist'>Watchlist</Link></div></li>
-              <li><div className="nav-link px-2 link-secondary nav-item"><Link className="nav-link active"  style={{ textDecoration: 'none' }} to='/movies'>Movies</Link></div></li>
+              <li><div className="nav-link px-2 link-secondary nav-item"><Link className="nav-link active" style={{ textDecoration: 'none' }} to='/movies'>Movies</Link></div></li>
               <li><div className="nav-link px-2 link-secondary nav-item"><Link className="nav-link active" style={{ textDecoration: 'none' }} to='/tvshows'>TV Shows</Link></div></li>
             </ul>
 
-            <div className="dropdown">
-            <button type="button" className="btn btn-danger">Dropdown</button>
-              <div className="dropdown-content">
-                <a className="nav-link" href="#">Watchlist</a>
-                <Link to='/profile'><button>Profile</button></Link>
-                <Link to='/watchlist'><button>Watchlist</button></Link>
-                <Link to='/changepassword'><button>Change<br />Password</button></Link>
+            <div className="dropdown" style={{ marginRight: '60px' }}>
+              <button type="button" className="btn btn-danger">Dropdown</button>
+              <div className="dropdown-content" style={{ backgroundColor: '#1A1A1A' }}>
+                <Link to='/profile'><button className='btn text-start text-white' style={{ padding: '5px 5px', border:'0px'}}>Profile</button></Link>
+                <Link to='/watchlist'><button className='btn text-start text-white' style={{ padding: '5px 5px', border:'0px'}}>Watchlist</button></Link>
+                <Link to='/changepassword'><button className='btn text-start text-white' style={{ padding: '5px 5px', border:'0px' }}>Change<br />Password</button></Link>
                 {localStorage.getItem('auth-token')
-                ?<button onClick={()=>{localStorage.removeItem('auth-token');window.location.replace('/login')}}>Logout</button>
-              :<Link to='/login'><button>Login</button></Link>}
+                  ? <Link><button className='btn text-white text-start' style={{ padding: '5px 5px', border:'0px'}} onClick={() => { localStorage.removeItem('auth-token'); window.location.replace('/login') }}>Logout</button></Link>
+                  : <Link to='/login'><button className='btn text-start text-white' style={{ padding: '5px 5px', border:'0px'}}>Login</button></Link>}
               </div>
             </div>
+
           </div>
         </div>
       </nav>
