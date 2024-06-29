233c5e46d5788ca6152871c153235463a3fc530e
nimendra-ag <nimendraamiththa1@gmail.com>
2024-06-29 14:05:40 +0530

    Sign up page is complete

--- frontend/src/Pages/Signup/Signup.jsx
+++ frontend/src/Pages/Signup/Signup.jsx
@@ -34,7 +34,7 @@ const Signup = () => {
     }
 
     return (
-        <section className="portfolio section-padding" id="contact" style={{marginTop:'100px'}}>
+        <section  style={{marginTop:'100px', marginBottom:"100px"}}>
 
             <div className="container-fluid">
 
@@ -42,45 +42,50 @@ const Signup = () => {
                     style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.823), rgba(0,0,0,0.1)), url('https://picsum.photos/1920/1080')` }}>
                     <div className="row">
 
-                        <div className="col-md-6  text-primary mb-md-0 mb-3">
-                            <h1 style={{ fontSize: '60px' }}>Try FinWise free for<br />30 days</h1>
+                        <div className="col-md-6  text-white mb-md-0 mb-3">
+                            <h1 style={{ fontSize: '50px' }}>Unlock Exclusive Features with Your<br />CineBase Account</h1>
                             <br />
-                            <h2>The average FinWiser<br />saves $600 in their<br />first two months (and you<br />seem above average, honestly).</h2>
+
+                            <p class="h5"><i class="bi bi-check2-circle"></i> Join a movie lover's community for an unparalleled cinema experience</p>
+                            <br />
+                            <p class="h5"><i class="bi bi-check2-circle"></i>Get recommendations, save favorites, create watchlists, and stay updated.</p>
+                            <br />
+                            <p class="h5"><i class="bi bi-check2-circle"></i> Sign up for CineBase today and discover movie magic!.</p>
                         </div>
 
-                        <div className="col-md-6 bg-light p-4" style={{borderRadius: "15px"}}>
+                        <div className="col-md-6 p-4" style={{borderRadius: "8px", backgroundColor:'#141414'}}>
 
-                            <div className="mb-3 text-center" style={{ color: 'black' }}>
+                            <div className="mb-3 text-center text-white">
                                 <h1>Create Account</h1>
-                                <p style={{ fontSize: "16px" }}>to continue as a FinWiser</p>
+                                <p style={{ fontSize: "16px" }}>so you don't miss it!</p>
                             </div>
 
                             <div className="mb-3">
-                                <label className="mb-2" style={{ fontSize: '18px', color: 'black' }}>Your Name</label>
+                                <label className="mb-2 text-white" style={{ fontSize: '18px'}}>Your Name</label>
                                 <input type="text" name='username' value={formData.username} onChange={changeHandler} className="form-control" aria-label="Name" />
                             </div>
 
                             <div className="mb-3">
-                                <label className="mb-2" style={{ fontSize: '18px', color: 'black' }}>Your Email</label>
+                                <label className="mb-2 text-white" style={{ fontSize: '18px'}}>Your Email</label>
                                 <input type="email" name='email' value={formData.email} onChange={changeHandler} className="form-control" aria-label="Email" />
                             </div>
 
                             <div className="mb-3">
-                                <label className="mb-2" style={{ fontSize: '18px', color: 'black' }}>Your Password</label>
+                                <label className="mb-2 text-white" style={{ fontSize: '18px'}}>Your Password</label>
                                 <input type="password" name='password' value={formData.password} onChange={changeHandler} className="form-control" aria-label="Password" />
                             </div>
 
 
-                            <div className="border-1 ps-2" style={{ textAlign: "left" }}>
-                                <p className="">By sharing your details, you agree to our <a href="#">Terms of Service </a>
+                            <div className="border-1 ps-2 text-whiite-50" style={{ textAlign: "left" }}>
+                                <p className="">By sharing your details, you agree to our <a href="#" className='text-white-50'>Terms of Service </a>
                                     and
-                                    <a href="#"> Privacy Policy</a>.
+                                    <a href="#" className='text-white-50'> Privacy Policy</a>.
                                 </p>
                             </div>
                           
-                            <button type="submit" onClick={()=>{signup()}} className="btn btn-outline-primary text-center">Sign Up</button>
+                            <button type="submit" onClick={()=>{signup()}} className="btn btn-danger text-center">Sign Up</button>
                             
-                            <p style={{ fontSize: '16px', marginTop: "30px" }}>Already have an account? <Link style={{textDecoration: 'none'}} to='/login'>Signin here</Link></p>
+                            <p style={{ fontSize: '16px', marginTop: "30px" }} className='text-white-50'>Already have an account?&nbsp;&nbsp;<Link style={{ textDecoration: 'none', color:'white'}} to='/login'>Signin here</Link></p>
                         </div>
                     </div>
                 </div>
