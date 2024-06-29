af0f4a27edea1bd0eb26f373960f3416c081d69f
nimendra-ag <nimendraamiththa1@gmail.com>
2024-06-29 11:32:39 +0530

    watchlist item's styles added

--- frontend/src/Components/WatchlistCard/WatchlistCard.jsx
+++ frontend/src/Components/WatchlistCard/WatchlistCard.jsx
@@ -1,23 +1,40 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { MediaContext } from '../../Context/MediaContext'
 
 const WatchlistCard = (props) => {
     const {removeFromWatchlist} = useContext(MediaContext);
+    const [isHovered, setIsHovered] = useState(false);
 
     return (
         <>
             <div className="col col-md-4 col-lg-2 col-xl-2 mb-4">
-                <div className="card h-100">
+            <div className="card h-100" style={{ backgroundColor: 'rgb(26,26,26)', color: 'white' }}>
                     <Link to={`/media/${props.id}`} ><img src={props.cardImage} className="card-img-top" alt="..." /></Link>
-                    <div className="card-body">
-                        <h5 className="card-title">{props.name}</h5>
-                        <p><i className="bi bi-star-fill"></i> {props.rating}</p>
-                        <a className="btn btn-primary" onClick={()=>removeFromWatchlist(props.id)}><i className="bi bi-bookmark-plus"></i> remove from watchlist</a>
+                    <div className="card-body" style={{ paddingBottom: '0px' }}>
+                        <p style={{ fontSize: '14px' }} ><i className="bi bi-star-fill"></i> {props.rating}</p>
+                        <h5 style={{ fontSize: '18px' }} className="card-title">{props.name}</h5>
                     </div>
-                    <div className="card-footer">
-                        <a href={props.trailor} target='_blank' className="btn btn-primary"><i className="bi bi-play"></i> Trailor</a>
 
+
+                    <div className="card-footer text-center" style={{ paddingTop: '0px' }}>
+                        <a href="#" className="btn btn-primary" style={{ marginBottom: '10px', backgroundColor: 'rgb(44,44,44)', border: '0px' }} onClick={() => { removeFromWatchlist(props.id) }} ><i class="bi bi-bookmark-check"></i> Watchlist</a>
+                        <a
+                            href={props.trailor}
+                            target='_blank'
+                            className="btn btn-primary"
+                            style={{
+                                backgroundColor: 'transparent',
+                                border: '0px',
+                                backgroundColor: isHovered ? 'rgb(44, 44, 44)' : 'inherit',
+                                transition: 'color 0.3s',
+                                textDecoration: 'none' /* Remove underline */
+                            }}
+                            onMouseEnter={() => setIsHovered(true)}
+                            onMouseLeave={() => setIsHovered(false)}
+                        >
+                            <i className="bi bi-play"></i> Trailer
+                        </a>
                     </div>
                 </div>
             </div>
--- frontend/src/Pages/Watchlist/Watchlist.jsx
+++ frontend/src/Pages/Watchlist/Watchlist.jsx
@@ -13,21 +13,21 @@ const Watchlist = () => {
                 <div className="mt-5 text-start ms-5 me-5">
                   <h1 style={{ fontSize: '60px' }}>Your Watchlist</h1>
                   <p className="h4 pt-4">
-                    Keep track of your favorite films and discover new recommendations with Your Watchlist, where you can save and organize movies you want to watch next
-                  </p>
+                    Stay entertained by keeping track of your favorite films and TV shows on Your Watchlist! Discover new recommendations, 
+                    save your must-watch picks, and organize your viewing list all in one place.                  </p>
                 </div>
-                
+
               </div>
             </div>
             <hr style={{ borderWidth: '4px', color: 'white' }} />
           </div>
 
           <div className="container p-3">
-          <WatchlistItems />
+            <WatchlistItems />
           </div>
         </div>
 
-        
+
       </section>
     </>
   )
