d0d47572f72e3ed15d76637007aa53bcc3308b80
Nimendra Gunawardana <nimendraamiththa1@gmail.com>
2024-10-19 22:30:14 +0530

    feedback form updated

--- frontend/src/Components/FeedbackForm/FeedbackForm.jsx
+++ frontend/src/Components/FeedbackForm/FeedbackForm.jsx
@@ -3,31 +3,43 @@ import axios from 'axios';
 
 const FeedbackForm = () => {
     const [formContent, setFormContent] = useState({
-        name:"",
+        name: "",
         country: "",
         feedback: "",
     })
 
+    const [loading, setLoading] = useState(false);
+
     const changeHandler = (e) => {
-        setFormContent({...formContent, [e.target.name]: e.target.value})
+        setFormContent({ ...formContent, [e.target.name]: e.target.value })
     }
     const Add_Feedback = async () => {
+
+        setLoading(true);
         console.log(formContent);
 
-        try{
+        try {
             const response = await axios.post('http://localhost:4000/addfeedback', formContent, {
-                headers:{
+                headers: {
                     Accept: 'application/json',
                     'Content-Type': 'application/json',
                 },
             });
 
-            if (response.data.success){
+            if (response.data.success) {
                 alert("Feedback added successfully");
-            } else{
+                setLoading(false);
+                setFormContent({
+                    name: "",
+                    country: "",
+                    feedback: "",
+                })
+                window.location.reload();
+
+            } else {
                 alert("Feedback not added");
             }
-        } catch (error){
+        } catch (error) {
             console.log(error);
         }
     }
@@ -35,26 +47,37 @@ const FeedbackForm = () => {
         <>
             <div className="col-sm-12" style={{ padding: '0 100 0 100' }}>
 
-                    <div className="form-group mb-3">
-                        <label className="sr-only">Name</label>
-                        <input value={formContent.name} onChange={changeHandler} type="text" className="form-control" placeholder="Name"
-                            name="name" required />
-                    </div>
+                <div className="form-group mb-3">
+                    <label className="sr-only">Name</label>
+                    <input value={formContent.name} onChange={changeHandler} type="text" className="form-control" placeholder="Name"
+                        name="name" required />
+                </div>
 
-                    <div className="form-group mb-3">
-                        <label className="sr-only" >Country</label>
-                        <input value={formContent.country} onChange={changeHandler} type="text" className="form-control" placeholder="Your Country"
-                            name="country" required />
-                    </div>
+                <div className="form-group mb-3">
+                    <label className="sr-only" >Country</label>
+                    <input value={formContent.country} onChange={changeHandler} type="text" className="form-control" placeholder="Your Country"
+                        name="country" required />
+                </div>
 
-                    <div className="form-group mb-3">
-                        <label className="sr-only">Your Feedback</label>
-                        <textarea value={formContent.feedback} onChange={changeHandler} type="text" className="form-control" rows="5" placeholder="Your Feedback"
-                            name="feedback" required></textarea>
-                    </div>
+                <div className="form-group mb-3">
+                    <label className="sr-only">Your Feedback</label>
+                    <textarea value={formContent.feedback} onChange={changeHandler} type="text" className="form-control" rows="5" placeholder="Your Feedback"
+                        name="feedback" required></textarea>
+                </div>
 
-                    
-                    <button onClick={()=>{Add_Feedback()}} className='btn btn-danger'>Submit</button>
+
+                <button
+                    onClick={() => { Add_Feedback() }}
+                    className='btn btn-danger'
+                    disabled={loading}
+                >{loading ? (
+                    <div className="spinner-border spinner-border-sm text-light" role="status">
+                        <span className="visually-hidden">Loading...</span>
+                    </div>
+                ) : (
+                    'Submit'
+                )}
+                </button>
 
             </div>
         </>
